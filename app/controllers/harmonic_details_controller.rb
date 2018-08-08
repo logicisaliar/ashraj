902b995523f5b38595cb6e73c9a6dae168f67303
Jash Shah <jash.shah@live.com>
2018-08-08 18:05:07 +0530

    Generate HarmonicDetail index

--- app/controllers/harmonic_details_controller.rb
+++ app/controllers/harmonic_details_controller.rb
@@ -1,2 +1,8 @@
 class HarmonicDetailsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+def index
+  @harmonic_details = HarmonicDetail.all
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,14 @@ require 'csv'
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'hsn.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
-  puts row.to_hash
+  t = HarmonicDetail.new
+  t.hsn_chapter = row['hsn_chapter']
+  t.gst = row['gst']
+  t.save
+  puts "#{t.hsn_chapter}, #{t.gst} saved"
 end
+
+puts "There are now #{HarmonicDetail.count} rows in the transactions table"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
