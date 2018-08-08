71e8d29bf38ba98b80cc32737424645f8c75c8f7
Jash Shah <jash.shah@live.com>
2018-08-08 17:22:18 +0530

    Generate basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'hsn.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  puts row.to_hash
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
