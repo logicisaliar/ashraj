c555d1d425ea726e1d5579f715e893ef9e039a78
Jash Shah <jash.shah@live.com>
2018-08-09 14:38:16 +0530

    Make test for HarmonicDetail

--- test/controllers/harmonic_details_controller_test.rb
+++ test/controllers/harmonic_details_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class HarmonicDetailsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  # test "visiting the index" do
+  #   visit "/harmonic_details"
+  #   assert_selector "h1", text: "HSN Details"
   # end
 end
