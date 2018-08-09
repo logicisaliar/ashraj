a92f5191ac4d3a2a14e0dbc7a8c2dd0f27e9a2b9
Jash Shah <jash.shah@live.com>
2018-08-10 02:08:23 +0530

    Create category model, controller and test

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class CategoriesTest < ApplicationSystemTestCase
   test "categories - index" do
     visit "/categories"
-    assert_selector "h1", text: "Categories"
+    assert_selector "h2", text: "Hitting array"
   end
 
   test "categories - new" do
