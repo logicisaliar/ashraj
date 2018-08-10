93a8bdc9a893342a511b70c7a7114a81d585686b
Jash Shah <jash.shah@live.com>
2018-08-10 14:46:17 +0530

    Create Type model, controller and csv file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_195507) do
+ActiveRecord::Schema.define(version: 2018_08_10_081815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 2018_08_09_195507) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["harmonic_detail_id"], name: "index_harmonics_on_harmonic_detail_id"
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
