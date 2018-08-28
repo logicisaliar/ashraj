49748a47e1ab5be4a5fdd6a1be63741c1ea9b392
Jash Shah <jash.shah@live.com>
2018-08-28 19:56:06 +0530

    Make home page better

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,3 @@
 import "bootstrap";
-import { initUpdateNavbarOnScroll } from '../components/navbar';
-initUpdateNavbarOnScroll();
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'development', to: 'pages#development'
   resources :types, only: [:index]
   resources :packings, only: [:index]
   resources :uses, only: [:index]
