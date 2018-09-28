13bccb9f59de38a228b03ac39568f6512b571b27
Jash Shah <jash.shah@live.com>
2018-09-28 18:38:40 +0530

    Add fields to User model

--- db/migrate/20180927140022_add_columns_to_users.rb
+++ db/migrate/20180927140022_add_columns_to_users.rb
@@ -2,7 +2,10 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.2]
   def change
     add_reference :users, :company, foreign_key: true
     add_column :users, :first_name, :string
-    add_column :users, :dept, :integer
+    add_column :users, :last_name, :string
+    add_column :users, :photo, :string
+    add_column :users, :role, :integer
     add_column :users, :dob, :date
+    add_column :users, :aadhar, :integer
   end
 end
