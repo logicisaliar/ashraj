cbf9d45f4328d6e31b535d0e5995144cdf928867
Jash Shah <jash.shah@live.com>
2018-10-05 15:29:29 +0530

    Add relations to Company and User

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,7 @@ class Company < ApplicationRecord
   belongs_to :parent, class_name: "Company", optional: true
   has_one :child, class_name: "Company", foreign_key: :parent_id
   has_many :addresses
+  has_many :numbers
+  has_many :mails
   validates :name, uniqueness: true
 end
